7e906f997ad4f5528910e9e0376e80dcf87ead52
Lorenzockio <scribblemelon@gmail.com>
2026-04-17 12:59:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,18 +7,34 @@ const sg = await gulls.init(),
 
 Mouse.init() 
 
-const NUM_PARTICLES = 1, 
-      NUM_PROPERTIES = 4, 
+const NUM_PARTICLES = 4, 
+      NUM_PROPERTIES = 6, 
       state = new Float32Array( NUM_PARTICLES * NUM_PROPERTIES)
 
-for( let i = 0; i < NUM_PARTICLES * NUM_PROPERTIES; i+= NUM_PROPERTIES) {
+state[0] = -1;
+state[1] = -1 + Math.random() * 2;
+state[2] = 12;
+state[3] = -7 + Math.random() * 14;
+state[4] = 0;
+state[5] = 0; //pad
+
+for (let p = 1; p < NUM_PARTICLES; p++) {
+  const i = p * NUM_PROPERTIES;
+
+  state[i] = state[0];
+  state[i + 1] = state[1];
+  state[i + 2] = state[2];
+  state[i + 3] = state[3];
+  state[i + 4] = i; 
+  state[i + 5] = 0; //pad
+}
+/*for( let i = 0; i < NUM_PARTICLES * NUM_PROPERTIES; i+= NUM_PROPERTIES) {
   state[ i ] = -1
   state[ i + 1 ] = -1 + Math.random() * 2
   state[ i + 2 ] = 12
   state[ i + 3 ] = -7 + Math.random() * 14
 
-
-}
+}*/
 
 const state_b = sg.buffer( state ),
       frame_u = sg.uniform( 0 ),
